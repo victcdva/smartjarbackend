72b3d1e4c59c9b921ef69bcaa72fabfa42e1a40f
Víctor Córdova <theslapboy@gmail.com>
2020-12-11 00:45:15 -0800

    Adding connection to pythonanywhere

--- config.py
+++ config.py
@@ -3,4 +3,5 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_ECHO = False
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root@localhost/smartjardb"
+#SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root@localhost/smartjardb"
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://victcdvaa:!qG9v_YU@victcdvaa.mysql.pythonanywhere-services.com/victcdvaa$smartjardb"
